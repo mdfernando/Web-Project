c64101d2054ae614d6342d4568c840d08f172a80
mdfernando <97427995+mdfernando@users.noreply.github.com>
2022-02-20 00:26:43 +0530

    Add files via upload
    completed the form.

--- MD Fernando (23590)/Introduction.js
+++ MD Fernando (23590)/Introduction.js
@@ -1,27 +1,27 @@
 function enlargemem1()
 {
     document.getElementById("member1").style.fontSize=30;
-    document.getElementById("member1").style.color="orange";
+    document.getElementById("member1").style.color="yellow";
 }
 function enlargemem2()
 {
     document.getElementById("member2").style.fontSize=30;
-    document.getElementById("member2").style.color="orange";
+    document.getElementById("member2").style.color="yellow";
 }
 function enlargemem3()
 {
     document.getElementById("member3").style.fontSize=30;
-    document.getElementById("member3").style.color="orange";
+    document.getElementById("member3").style.color="yellow";
 }
 function enlargemem4()
 {
     document.getElementById("member4").style.fontSize=30;
-    document.getElementById("member4").style.color="orange";
+    document.getElementById("member4").style.color="yellow";
 }
 function enlargemem5()
 {
     document.getElementById("member5").style.fontSize=30;
-    document.getElementById("member5").style.color="orange";
+    document.getElementById("member5").style.color="yellow";
 }
 
 function resizemem1()
