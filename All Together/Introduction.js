aa91e6b8f7e28392cca4e1c1ec72977bf08b88ac
mdfernando <97427995+mdfernando@users.noreply.github.com>
2022-02-21 12:23:43 +0530

    Add files via upload

---
+++ All Together/Introduction.js
@@ -0,0 +1,51 @@
+function enlargemem1()
+{
+    document.getElementById("member1").style.fontSize=30;
+    document.getElementById("member1").style.color="yellow";
+}
+function enlargemem2()
+{
+    document.getElementById("member2").style.fontSize=30;
+    document.getElementById("member2").style.color="yellow";
+}
+function enlargemem3()
+{
+    document.getElementById("member3").style.fontSize=30;
+    document.getElementById("member3").style.color="yellow";
+}
+function enlargemem4()
+{
+    document.getElementById("member4").style.fontSize=30;
+    document.getElementById("member4").style.color="yellow";
+}
+function enlargemem5()
+{
+    document.getElementById("member5").style.fontSize=30;
+    document.getElementById("member5").style.color="yellow";
+}
+
+function resizemem1()
+{
+    document.getElementById("member1").style.fontSize=20;
+    document.getElementById("member1").style.color="white";
+}
+function resizemem2()
+{
+    document.getElementById("member2").style.fontSize=20;
+    document.getElementById("member2").style.color="white";
+}
+function resizemem3()
+{
+    document.getElementById("member3").style.fontSize=20;
+    document.getElementById("member3").style.color="white";
+}
+function resizemem4()
+{
+    document.getElementById("member4").style.fontSize=20;
+    document.getElementById("member4").style.color="white";
+}
+function resizemem5()
+{
+    document.getElementById("member5").style.fontSize=20;
+    document.getElementById("member5").style.color="white";
+}
